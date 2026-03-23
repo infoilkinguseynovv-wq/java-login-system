67e6c758619f894a19ff254f6d85963b6b9e2452
infoilkinguseynovv-wq <infoilkinguseynovv@gmail.com>
2026-03-23 21:59:54 +0400

    Add login endpoint to LoginController

--- src/main/java/az/logintest/LoginController.java
+++ src/main/java/az/logintest/LoginController.java
@@ -1,4 +1,17 @@
 package az.logintest;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
 public class LoginController {
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody Map<String, String> body) {
+        String username = body.get("username");
+        String password = body.get("password");
+        return LoginService.login(username, password);
+    }
 }
